31cead584d16f0e54a66341527caacb25d1eb02f
YeePomfPomf <131655763+yeepomfpomf@users.noreply.github.com>
2025-12-15 19:29:39 +1100

    Messing around with NavMesh
    added navmesh to the ship

--- Enemy/Sealion/Sealion.gd
+++ Enemy/Sealion/Sealion.gd
@@ -1,2 +1,24 @@
 extends CharacterBody3D
 class_name Sealion
+
+
+@export var player: Player
+
+var state: SealionBaseState = SealionStates.SWIMMING
+
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
+var speed:= 2
+
+
+func _ready() -> void:
+	state.enter(self)
+
+func _physics_process(delta: float) -> void:
+	nav_agent.target_position = player.global_position
+	var dir: Vector3 = (nav_agent.get_next_path_position() - global_position).normalized()
+	velocity = Vector3(dir.x,0,dir.z) * speed
+	
+	if not is_on_floor():
+		velocity.y += get_gravity().y * delta
+	
+	move_and_slide()
--- Enemy/Sealion/StateMachine/States/SealionBaseState.gd
+++ Enemy/Sealion/StateMachine/States/SealionBaseState.gd
@@ -2,14 +2,14 @@ extends RefCounted
 class_name SealionBaseState
 
 
-func enter(sealion: Sealion) -> void:
+func enter(_sealion: Sealion) -> void:
 	pass
 
-func exit(sealion: Sealion) -> void:
+func exit(_sealion: Sealion) -> void:
 	pass
 
-func pre_update(sealion: Sealion) -> void:
+func pre_update(_sealion: Sealion) -> void:
 	pass
 
-func update(sealion: Sealion) -> void:
+func update(_sealion: Sealion) -> void:
 	pass
