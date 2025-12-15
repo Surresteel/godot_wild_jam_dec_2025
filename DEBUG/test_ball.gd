e674078115030ba4a87798f25973ad6460f3e664
Surresteel <Surresteel@gmail.com>
2025-12-15 16:33:59 +1100

    Updated buoyancy offsets for ship.

--- DEBUG/test_ball.gd
+++ DEBUG/test_ball.gd
@@ -4,4 +4,4 @@ func _process(_delta: float) -> void:
 	var data := Vector3(global_position.x, global_position.z, 
 				(Time.get_ticks_msec() / 1000.0))
 	var water_height = NoiseFunc.sample_at_pos_time(data)
-	transform.origin.y = water_height
+	global_position.y = water_height
